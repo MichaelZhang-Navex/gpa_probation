d66c58a7f27714001c46bbbf3b24d70bb69cec32
Michael Zhang <michael.zhang@navex.com>
2024-06-13 16:30:28 +0000

    devcontainer

---
+++ .devcontainer/.zshrc
@@ -0,0 +1,9 @@
+export ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="bira"
+
+plugins=(git direnv)
+
+source $ZSH/oh-my-zsh.sh
+DISABLE_AUTO_UPDATE=true
+DISABLE_UPDATE_PROMPT=true
+
---
+++ .devcontainer/init.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+{
+  inherit (pkgs)
+    direnv
+    neovim
+    nodejs_20
+    gh
+    git
+    yarn
+    aws-sam-cli
+    awscli2
+    fzf
+    bat
+    ripgrep
+;
+}
